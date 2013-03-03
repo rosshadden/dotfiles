170c6e3b01820cc430f957841a0170cde2495aae
Ross Hadden <rosshadden@gmail.com>
2013-03-02 22:58:07 -0500

    Hi, Mom!

---
+++ alias.sh
@@ -0,0 +1,8 @@
+alias copy='xclip -selection c'
+alias paste='xclip -o -selection CLIPBOARD'
+
+alias o='optirun'
+alias ross-git-merge="git checkout master && git merge dev && git checkout dev && git push origin dev && git push origin master"
+alias suspend="dbus-send --print-reply --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend"
+
+alias ross="~/Dropbox/.dots/r.js"
---
+++ path.sh
@@ -0,0 +1,16 @@
+export DEFAULT_USER=$USER
+
+export _NO_RESOLVE_SYMLINKS=true
+. /home/$USER/Dropbox/.dots/z/z.sh
+function precmd () {
+	_z --add "$(pwd)"
+}
+
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export PATH=$PATH:~/media/programs/android-sdk/tools:~/media/programs/android-sdk/platform-tools:$JAVA_HOME/bin
+export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64
+
+export PATH=$PATH:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib:/usr/local/cuda/lib64
+echo 'export PATH=$PATH:/usr/local/cuda/bin' >> ~/.bash_profile
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib:/usr/local/cuda/lib64' >> ~/.bash_profile
