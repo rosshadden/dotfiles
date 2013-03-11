b76a7239e2be2a8e5246c8ac6016bc2fb850310a
Ross Hadden <rosshadden@gmail.com>
2013-03-11 09:44:39 -0400

    Set r.js up to use commander.js

--- alias.sh
+++ alias.sh
@@ -6,4 +6,4 @@ alias o='optirun'
 alias ross-git-merge="git checkout master && git merge dev && git checkout dev && git push origin dev && git push origin master"
 alias suspend="dbus-send --print-reply --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend"
 
-alias ross="~/Dropbox/.dots/r.js"
+alias ross="~/Dropbox/.dots/r/r.js"
--- path.sh
+++ path.sh
@@ -6,7 +6,8 @@ function precmd () {
 	_z --add "$(pwd)"
 }
 
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+# export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
 export PATH=$PATH:~/media/programs/android-sdk/tools:~/media/programs/android-sdk/platform-tools:$JAVA_HOME/bin
 export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64
 
