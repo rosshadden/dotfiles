72c2fce77e67bb8d95ad5a14924b2ea64d132ff9
Ross Hadden <rosshadden@gmail.com>
2013-03-18 14:21:37 -0400

    Added zscript.

--- ross/ross.js
+++ ross/ross.js
@@ -2,18 +2,21 @@
 
 var sys = require('sys'),
 	exec = require('child_process').exec,
-	program = require('commander');
+	program = require('commander'),
+	Lazy = require('lazy');
 
 var puts = function(error, stdout, stderr){
 	if(stdout.length){
 		sys.puts(stdout);
+	}
+	if(stderr.length){
 		sys.puts(stderr);
 	}
 };
 
 var run = function(){
 	var args = Array.prototype.slice.call(arguments);
-	var handler = (typeof args[args.length - 1] === 'function') ? args.splice(-1, 1) : puts;
+	var handler = (typeof args[args.length - 1] === 'function') ? args.splice(-1, 1)[0] : puts;
 	args.forEach(function(arg, a){
 		exec(arg, handler);
 	});
@@ -58,10 +61,32 @@ program
 	.command('search <pattern> <path>')
 	.description('Search files using RegEx.')
 	.action(function(pattern, path){
-		var expression = new RegExp(pattern);
-		run('egrep ' + pattern + ' ' + path, function(){
-			var args = Array.prototype.slice.call(arguments);
-			console.log('response', args);
+		var expression = new RegExp(pattern, 'gm');
+		var command = 'egrep -n "' + pattern + '" ' + path;
+		run(command, function(error, stdout, stderr){
+			var lines;
+			if(stdout.length){
+				lines = stdout.split('\n');
+				lines = lines.map(function(line, l){
+					var preamble = line.match(/^.+:\d+:/);
+					if(preamble){
+						var offset = line.indexOf(preamble[0]);
+						var rest = line.substr(offset);
+						console.log('rest', rest);
+						var matches = expression.exec(rest);
+						if(matches){
+							return {
+								preamble: preamble[0],
+								matches: matches.slice(1, -2)
+							};
+						}
+					}
+					return {};
+				});
+				lines.forEach(function(line, l){
+					console.log(line);
+				});
+			}
 		});
 	});
 
