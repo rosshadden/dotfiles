d9c62b912b4ca78820af14438be5f14fac80619d
Ross Hadden <rosshadden@gmail.com>
2014-12-13 14:14:07 -0500

    Added es6 support in the grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 var path = require('path'),
 	loadGruntConfig = require('load-grunt-config');
 
+require('6to5/register');
+
+
 module.exports = function(grunt) {
 	loadGruntConfig(grunt, {
 		init: true,
--- tasks/install.js
+++ tasks/install.js
@@ -1,3 +1,21 @@
+var shell = require('shelljs');
+
+
 module.exports = function(grunt) {
-	grunt.registerTask('install', ['symlink']);
+	grunt.registerTask('install', function() {
+		const HOME = process.env.HOME;
+
+		let dirs = {
+			zsh: `${HOME}/.oh-my-zsh`,
+			dots: `${HOME}/dotfiles`,
+			db: `${HOME}/Dropbox`,
+		};
+
+
+		// submodules
+		// shell.exec('git submodule init');
+		// shell.exec('git submodule update');
+
+		grunt.task.run('symlink');
+	});
 };
