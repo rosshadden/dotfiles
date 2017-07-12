e24dac75ef267c2bab831078e3ea2dbc44af24a0
Ross Hadden <rosshadden@gmail.com>
2017-07-12 16:59:25 -0400

    Started down the path of using both surfingkeys and mouseless.

--- src/.config/surfingkeys/config.js
+++ src/.config/surfingkeys/config.js
@@ -1,48 +1,72 @@
-Hints.characters = 'aoeusnthidkbpg';
-
-// tabs
-
-map('<Alt-p>', 'E');
-map('<Alt-n>', 'R');
-imap('<Alt-p>', 'E');
-imap('<Alt-n>', 'R');
-unmap('E');
-unmap('R');
-
-map('<Alt-c>', 'x');
-unmap('x');
-
-map('<Alt-Tab>', 'B');
-map('<Alt-Shift-Tab>', 'F');
-
-map('gm', '<Alt-m>');
-
-// scrolling
-
-map('<Ctrl-b>', 'e');
-map('<Ctrl-u>', 'e');
-map('<Ctrl-f>', 'd');
-map('<Ctrl-d>', 'd');
-unmap('e');
-unmap('d');
-
-map('gp', '<Alt-p>');
-
-map('gD', 'yt');
-unmap('yt');
-
-// history
-
-map('J', 'H');
-
-map('H', 'S');
-map('L', 'D');
-
-// INSERT
-
-iunmap('<Ctrl-f>');
-
 map('<Ctrl-o>', '<Ctrl-i>');
-imap('<Ctrl-o>', '<Ctrl-i>');
-unmap('<Ctrl-i>');
-iunmap('<Ctrl-i>');
+
+unmapAllExcept([
+	'<Ctrl-o>',
+	':'
+]);
+
+function toggleQuote() {
+	var val = document.activeElement.value;
+	if (val[0] === '"') {
+		document.activeElement.value = val.substr(1, val.length - 2);
+	} else {
+		document.activeElement.value = '"' + val + '"';
+	}
+}
+imapkey("<Ctrl-'>", '#15Toggle quotes in an input element', toggleQuote);
+cmapkey("<Ctrl-'>", '#15Toggle quotes in an input element', toggleQuote);
+imapkey('<Ctrl-o>', '#15Open vim editor for current input', function() {
+	var element = document.activeElement;
+	Front.showEditor(element, function(data) {
+		$(element).val(data);
+	}, element.localName);
+});
+
+// Hints.characters = 'aoeusnthidkbpg';
+//
+// // tabs
+//
+// map('<Alt-p>', 'E');
+// map('<Alt-n>', 'R');
+// imap('<Alt-p>', 'E');
+// imap('<Alt-n>', 'R');
+// unmap('E');
+// unmap('R');
+//
+// map('<Alt-c>', 'x');
+// unmap('x');
+//
+// map('<Alt-Tab>', 'B');
+// map('<Alt-Shift-Tab>', 'F');
+//
+// map('gm', '<Alt-m>');
+//
+// // scrolling
+//
+// map('<Ctrl-b>', 'e');
+// map('<Ctrl-u>', 'e');
+// map('<Ctrl-f>', 'd');
+// map('<Ctrl-d>', 'd');
+// unmap('e');
+// unmap('d');
+//
+// map('gp', '<Alt-p>');
+//
+// map('gD', 'yt');
+// unmap('yt');
+//
+// // history
+//
+// map('J', 'H');
+//
+// map('H', 'S');
+// map('L', 'D');
+//
+// // INSERT
+//
+// iunmap('<Ctrl-f>');
+//
+// map('<Ctrl-o>', '<Ctrl-i>');
+// imap('<Ctrl-o>', '<Ctrl-i>');
+// unmap('<Ctrl-i>');
+// iunmap('<Ctrl-i>');
