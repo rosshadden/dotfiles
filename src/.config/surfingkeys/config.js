d77cb4e6cb20f7281e6466564dab760724a861e3
Ross Hadden <rosshadden@gmail.com>
2017-07-12 16:41:25 -0400

    Added surfingkeys.

---
+++ src/.config/surfingkeys/config.js
@@ -0,0 +1,48 @@
+Hints.characters = 'aoeusnthidkbpg';
+
+// tabs
+
+map('<Alt-p>', 'E');
+map('<Alt-n>', 'R');
+imap('<Alt-p>', 'E');
+imap('<Alt-n>', 'R');
+unmap('E');
+unmap('R');
+
+map('<Alt-c>', 'x');
+unmap('x');
+
+map('<Alt-Tab>', 'B');
+map('<Alt-Shift-Tab>', 'F');
+
+map('gm', '<Alt-m>');
+
+// scrolling
+
+map('<Ctrl-b>', 'e');
+map('<Ctrl-u>', 'e');
+map('<Ctrl-f>', 'd');
+map('<Ctrl-d>', 'd');
+unmap('e');
+unmap('d');
+
+map('gp', '<Alt-p>');
+
+map('gD', 'yt');
+unmap('yt');
+
+// history
+
+map('J', 'H');
+
+map('H', 'S');
+map('L', 'D');
+
+// INSERT
+
+iunmap('<Ctrl-f>');
+
+map('<Ctrl-o>', '<Ctrl-i>');
+imap('<Ctrl-o>', '<Ctrl-i>');
+unmap('<Ctrl-i>');
+iunmap('<Ctrl-i>');
