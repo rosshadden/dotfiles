78a7976a08024b081390607acac216e272bbefc1
Ross Hadden <rosshadden@gmail.com>
2023-06-19 16:52:13 -0400

    Modularized kanata config, using changes on my fork of kanata.
    https://github.com/jtroo/kanata/pull/456

--- src/.config/nvim/lua/core/modules/completion.lua
+++ src/.config/nvim/lua/core/modules/completion.lua
@@ -122,7 +122,7 @@ cmp.setup.cmdline(":", {
 
 -- codeium
 vim.g.codeium_disable_bindings = true
-vim.keymap.set("i", "<cr>", function () return vim.fn["codeium#Accept"]() end, { expr = true })
+vim.keymap.set("i", "<c-b>", function () return vim.fn["codeium#Accept"]() end, { expr = true })
 vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
 vim.keymap.set("i", "<m-]>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
 vim.keymap.set("i", "<m-[>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
