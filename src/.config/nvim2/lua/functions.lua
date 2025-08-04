cab80e333755deb874c30f08ebc9bbd2ec8beb84
Ross Hadden <rosshadden@gmail.com>
2025-08-04 15:52:54 -0400

    added pack fn

--- src/.config/nvim2/lua/functions.lua
+++ src/.config/nvim2/lua/functions.lua
@@ -17,3 +17,54 @@ function map(seq, action, cfg)
 
 	vim.keymap.set(modes, seq, action, options)
 end
+
+local function normalize_package(pkg)
+  if type(pkg) == 'string' then
+    return { src = pkg:match('^https?://') and pkg or ('https://github.com/' .. pkg) }
+  elseif type(pkg) == 'table' then
+    -- { "user/repo", opts = { ... } }
+    if pkg[1] and type(pkg[1]) == 'string' then
+      local out = {
+        src = pkg[1]:match('^https?://') and pkg[1] or ('https://github.com/' .. pkg[1])
+      }
+      if pkg.opts then
+        for k, v in pairs(pkg.opts) do out[k] = v end
+      end
+      if pkg.deps then out.deps = pkg.deps end
+      return out
+    elseif pkg.src then
+      return vim.deepcopy(pkg)
+    else
+      error("invalid package spec")
+    end
+  else
+    error("invalid package spec")
+  end
+end
+
+function pack(...)
+  local args = { ... }
+  local final = {}
+  local seen = {}
+
+  local function add(pkg)
+    pkg = normalize_package(pkg)
+
+    if seen[pkg.src] then return end
+    seen[pkg.src] = true
+
+    if pkg.deps then
+      for _, dep in ipairs(pkg.deps) do
+        add(dep)  -- raw strings like "user/repo" get normalized here
+      end
+    end
+
+    table.insert(final, pkg)
+  end
+
+  for _, pkg in ipairs(args) do
+    add(pkg)
+  end
+
+  vim.pack.add(final)
+end
